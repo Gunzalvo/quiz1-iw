50e33d0be0ec991115142a82606d26efa07f35fd
Gonzalo_Leon <g.leonesteban@gmail.com>
2021-04-28 16:07:37 -0400

    Actualizar notas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-
-  notes : Array<Object> = [];
+	
+  notes : Array<Object> = new Array();
   overlay : HTMLElement | null = null;
+  selected : number = 0;
   buttonCreate : HTMLElement | null = null;
   buttonUpdate : HTMLElement | null = null;
   buttonCancel : HTMLElement | null = null;
@@ -50,6 +51,8 @@ export class HomeComponent implements OnInit {
 		
 		if( !error ) {
 			var div : HTMLElement = document.createElement("div");
+			var index : number = this.notes.length;
+			div.addEventListener("click", ()=>{ this.selectNote(index)}, false);
 			div.className = "note";
 			
 			var h3 : HTMLElement = document.createElement("h3");
@@ -79,25 +82,102 @@ export class HomeComponent implements OnInit {
 				
 				if( cont != null )
 					cont.appendChild(div);
+				
+				var note : Object = {
+					el: div,
+					title: this.noteTitle.value,
+					status: this.noteStatus.selectedIndex
+				}
+				this.notes.push(note);
 			}
 			
 			this.cancel();
+			this.noteTitle.value = "";
 		}
 	}
 	
+  }
+  
+  selectNote(i:number) : void {
+	if( this.buttonCreate != null )
+		this.buttonCreate.style.display = "none";
 	
+	if( this.buttonCancel != null )
+		this.buttonCancel.style.display = "none";
+	
+	if( this.buttonUpdate != null )
+		this.buttonUpdate.style.display = "inline-block";
+	
+	var note : any | null = this.notes[i];
+	if( note != null ) {
+		this.selected = i;
+		
+		var title : string = note.title;
+		var status : number = parseInt(note.status);
+		
+		if( this.noteTitle != null )
+			this.noteTitle.value = title;
+		
+		if( this.noteStatus != null ) {
+			var op : HTMLElement = this.noteStatus.children[status] as HTMLElement;
+			this.noteStatus.value = op.innerText;
+		}
+	}
+	
+	this.showDialog();
   }
   
   updateNote() : void {
-	  
+	var note : any | null = this.notes[this.selected];
+	if( note != null ) {
+		
+		if( this.noteTitle != null )
+			note.title = this.noteTitle.value;
+		
+		if( this.noteStatus != null ) {
+			note.status = this.noteStatus.selectedIndex;
+			var cont : HTMLElement | null = null;
+			switch( note.status ) {
+				case 0:
+					cont = document.getElementById("started-notes");
+					break;
+				
+				case 1:
+					cont = document.getElementById("process-notes");
+					break;
+				case 2:
+					cont = document.getElementById("ready-notes");
+					break;
+			}
+			
+			if( cont != null )
+				cont.appendChild(note.el);
+		}
+		
+		var h3 : HTMLElement | null = note.el.firstElementChild;
+		if( h3 != null )
+			h3.innerText = note.title;
+	}
+	
+	this.cancel();
+  }
+  
+  newNote() : void {
+	if( this.buttonCreate != null )
+		this.buttonCreate.style.display = "inline-block";
+	
+	if( this.buttonCancel != null )
+		this.buttonCancel.style.display = "inline-block";
+	
+	if( this.buttonUpdate != null )
+		this.buttonUpdate.style.display = "none";
+	 
+	this.showDialog();
   }
   
   showDialog() : void {
-	  
-	 if( this.buttonUpdate != null )
-		this.buttonUpdate.style.display = "none";
-	  
-    if( this.overlay != null) {
+	
+	if( this.overlay != null) {
 		this.overlay.style.display = "flex";
 		
 		let tid;
